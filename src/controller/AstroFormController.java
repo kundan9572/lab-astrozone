395970c17f56fedfecc6007092b38d558b2516f0
kundan kumar <kd.kundan9572@gmail.com>
2020-09-25 23:37:28 +0530

    Completed

--- src/controller/AstroFormController.java
+++ src/controller/AstroFormController.java
@@ -12,87 +12,93 @@ import javax.servlet.http.HttpServletResponse;
 import model.User;
 import service.AstroCalculator;
 
-
-@WebServlet(urlPatterns = {"/entervalue"})
+@WebServlet(urlPatterns = { "/entervalue" })
 public class AstroFormController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/entervalues.jsp");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/entervalues.jsp");
 		rd.forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
- 
-		String name=request.getParameter("name");
-		String gender=request.getParameter("gender");
-		String date=request.getParameter("dob1");
-		String month=request.getParameter("dob2");
-		String year=request.getParameter("dob3");
-		String dob = date+"-"+month+"-"+year;	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String name = request.getParameter("name");
+		String gender = request.getParameter("gender");
+		String date = request.getParameter("dob1");
+		String month = request.getParameter("dob2");
+		String year = request.getParameter("dob3");
+		String dob = date + "-" + month + "-" + year;
+
+        //*Create an object for User class in the AstroFormController and pass the values given above as arguments to constructor.*
+		//*Create an object for the AstroCalculator class.*
+		//*Call the findSign method and store the return value in a String variable called as astrosign*
+		User  u=new User(name,date,month);
+		AstroCalculator ac=new AstroCalculator();
+		String astrosign=ac.findSign(u);
 		
-		User  obj1=new User(name,gender, dob, dob);
-		AstroCalculator  obj2=new AstroCalculator();
-		String astrosign=obj2.findSign();
-		
-		if(astrosign.equals("Aquarius")){
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
+			System.out.println(astrosign);
+
+		if (astrosign.equals("Aquarius")) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/aquarius.svg'>");
 			rd.forward(request, response);
 		}
-		
-		if(astrosign.equals("Aries")){
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
+
+		if (astrosign.equals("Aries")) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/aries.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Cancer")){
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
+		if (astrosign.equals("Cancer")) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/cancer.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Capricorn")){
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
+		if (astrosign.equals("Capricorn")) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/capricorn.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Gemini")){
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
+		if (astrosign.equals("Gemini")) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/gemini.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Leo")){
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
+		if (astrosign.equals("Leo")) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/leo.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Libra")){
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
+		if (astrosign.equals("Libra")) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/libra.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Pisces")){
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
+		if (astrosign.equals("Pisces")) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/pisces.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Sagittarius")){
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
+		if (astrosign.equals("Sagittarius")) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/sagittaruis.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Scorpio")){
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
+		if (astrosign.equals("Scorpio")) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/scorpio.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Taurus")){
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
+		if (astrosign.equals("Taurus")) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/taurus.svg'>");
 			rd.forward(request, response);
 		}
-		if(astrosign.equals("Virgo")){
-			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
+		if (astrosign.equals("Virgo")) {
+			RequestDispatcher rd = this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/virgo.svg'>");
 			rd.forward(request, response);
 		}
--- src/model/User.java
+++ src/model/User.java
@@ -1,43 +1,19 @@
 package model;
 
-
-public class User{
-	String name;
-	String email;
-	String dob;
-	String gender;
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getDob() {
-		return dob;
-	}
-	public void setDob(String dob) {
-		this.dob = dob;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public User(String name, String email, String dob, String gender) {
-		super();
-		this.name = name;
-		this.email = email;
-		this.dob = dob;
-		this.gender = gender;
-	}
+//Type your code here
+public class User
+{
+	public String name;
+	 public int d;
+	public int m;
 	
 	
+	
+	public User(String name,String date,String month)
+	{
+		this.name=name;
+		d=Integer.parseInt(date);
+		m=Integer.parseInt(month);
+	}
 	
 }
